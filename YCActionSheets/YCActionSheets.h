5dc05e9f159adb736a34cde00669a98193f61ce5
Y.Chudnick <y.chudnovets@temabit.com>
2019-01-11 13:37:51 +0200

    Initial Commit

---
+++ YCActionSheets/YCActionSheets.h
@@ -0,0 +1,19 @@
+//
+//  YCActionSheets.h
+//  YCActionSheets
+//
+//  Created by Yurii Chudnovets on 1/11/19.
+//  Copyright © 2019 Yurii Chudnovets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YCActionSheets.
+FOUNDATION_EXPORT double YCActionSheetsVersionNumber;
+
+//! Project version string for YCActionSheets.
+FOUNDATION_EXPORT const unsigned char YCActionSheetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YCActionSheets/PublicHeader.h>
+
+
