0722c68966b573dacb1e00b7d0b3e6e4fad679d1
Y.Chudnick <y.chudnovets@temabit.com>
2019-01-22 16:49:52 +0200

    change transaction style

--- YCActionSheets/YCActionSheetVC.swift
+++ YCActionSheets/YCActionSheetVC.swift
@@ -10,14 +10,15 @@ import UIKit
 
 public class YCActionSheetVC: UIViewController {
     
+    // MARK: - Properties
+    
     /// Background color.
-    public var backColor: UIColor = UIColor.black.withAlphaComponent(0.5) {
-        didSet {
-            view.backgroundColor = backColor
-        }
-    }
+    public var backColor: UIColor = UIColor.black.withAlphaComponent(0.5)
     
     var standardSpacing: CGFloat = 60
+    
+    private let animationDuration: TimeInterval = 0.3
+    private var isPresenting = false
     
     let conteinerView: UIView = UIView()
     private var conteinerViewHeight: CGFloat = 380
@@ -40,11 +41,13 @@ public class YCActionSheetVC: UIViewController {
     //MARK: - Setup presentation style
     
     fileprivate func setupPresentationStyle() {
-        modalPresentationStyle = .overFullScreen
-        modalTransitionStyle = .crossDissolve
+        modalPresentationStyle = .custom
+        transitioningDelegate = self
     }
+    
+    // MARK: - View lifecycle
 
-    override public func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         setupViews()
@@ -59,7 +62,7 @@ public class YCActionSheetVC: UIViewController {
     //MARK: Setup views
     
     fileprivate func setupViews() {
-        view.backgroundColor = backColor
+        view.backgroundColor = .clear
         
         view.addSubview(conteinerView)
         conteinerView.translatesAutoresizingMaskIntoConstraints = false
@@ -116,4 +119,69 @@ public class YCActionSheetVC: UIViewController {
         return result
     }
 
+}
+
+extension YCActionSheetVC: UIViewControllerTransitioningDelegate {
+    
+    public func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = true
+        
+        return self
+    }
+    
+    public func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = false
+        
+        return self
+    }
+    
+}
+
+extension YCActionSheetVC: UIViewControllerAnimatedTransitioning {
+    
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return isPresenting ? 0 : animationDuration
+    }
+    
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        
+        let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
+        let fromView = fromViewController.view
+        
+        let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
+        let toView = toViewController.view
+        
+        if isPresenting {
+            toView?.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            containerView.addSubview(toView!)
+            
+            transitionContext.completeTransition(true)
+            presentView(toView!, presentingView: fromView!, animationDuration: animationDuration, completion: nil)
+        } else {
+            dismissView(fromView!, presentingView: toView!, animationDuration: animationDuration) { completed in
+                if completed {
+                    fromView?.removeFromSuperview()
+                }
+                transitionContext.completeTransition(completed)
+            }
+        }
+    }
+    
+    private func presentView(_ presentedView: UIView, presentingView: UIView, animationDuration: Double, completion: ((_ completed: Bool) -> Void)?) {
+        UIView.animate(withDuration: animationDuration, animations: { [weak self] in
+            self?.view.backgroundColor = self?.backColor
+        }) { finished in
+            completion?(finished)
+        }
+    }
+    
+    private func dismissView(_ presentedView: UIView, presentingView: UIView, animationDuration: Double, completion: ((_ completed: Bool) -> Void)?) {
+        UIView.animate(withDuration: animationDuration, animations: { [weak self] in
+            self?.view.backgroundColor = UIColor.clear
+        }) { _ in
+            completion?(true)
+        }
+    }
+    
 }
